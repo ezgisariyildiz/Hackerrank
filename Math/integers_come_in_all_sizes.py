58357a727943cf1a8270def2ee076601c05141a8
Ezgi Çağla Sarıyıldız <e.sariyildiz2018@gtu.edu.tr>
2024-02-26 14:07:08 +0300

    26.02.2024

---
+++ Math/integers_come_in_all_sizes.py
@@ -0,0 +1,11 @@
+# Dört tam sayıyı al
+a = int(input().strip())
+b = int(input().strip())
+c = int(input().strip())
+d = int(input().strip())
+
+# ab + cd ifadesini hesapla
+result = pow(a, b) + pow(c, d)
+
+# Sonucu yazdır
+print(result)
