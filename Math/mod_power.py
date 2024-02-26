bae8c16cb2e8ee811e2e6866f55c64ca2e037303
Ezgi Çağla Sarıyıldız <e.sariyildiz2018@gtu.edu.tr>
2024-02-26 13:52:15 +0300

    26.02.2024

---
+++ Math/mod_power.py
@@ -0,0 +1,8 @@
+# a, b ve m değerlerini al
+a = int(input().strip())
+b = int(input().strip())
+m = int(input().strip())
+
+# a**b ve a**b % m sonuçlarını yazdır
+print(pow(a, b))
+print(pow(a, b, m))
