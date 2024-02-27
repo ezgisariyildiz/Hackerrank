557752434c40e660e33fdee1e76133119d20d647
Ezgi Çağla Sarıyıldız <e.sariyildiz2018@gtu.edu.tr>
2024-02-27 14:20:30 +0300

    27.02.2024

--- Itertools/maximize_it.py
+++ Itertools/maximize_it.py
@@ -4,10 +4,15 @@ K, M = map(int, input().split())
 
 lists = []
 for _ in range(K):
+    ''' Her döngüde, boşluklarla ayrilmis olarak alinan girdinin
+    ikinci satirindan başlayarak elemanlari alir 
+    ve bunlari integer'a dönüştürerek lists listesine ekler'''
     lists.append(list(map(int, input().split()[1:])))
 
 max_val = 0
+# lists listesindeki her bir listenin elemanları arasından her bir kombinasyonu alır
 for combination in product(*lists):
+    # Her bir kombinasyonun elemanlarının karelerini alıp toplar ve M'ye göre modunu alarak denklemin sonucunu bulur
     s = sum(x ** 2 for x in combination) % M
     if s > max_val:
         max_val = s
