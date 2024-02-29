008b045bc5bd0ee52436e9fd0f2f43fc1c4c3bc9
Ezgi Çağla Sarıyıldız <e.sariyildiz2018@gtu.edu.tr>
2024-02-29 16:14:46 +0300

    29.02.2024

--- RegexAndParsing/re_findall_re_finditer.py
+++ RegexAndParsing/re_findall_re_finditer.py
@@ -1,16 +1,19 @@
 import re
 
 def find_vowel_substrings(s):
+    # En az 2 sesli harf içeren alt dizeleri bulmak için regex kalıbı
     pattern = r'(?<=[qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM])[aeiouAEIOU]{2,}(?=[qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM])'
     
+    # Desenle eşleşen tüm alt dizeleri bul
     substrings = re.findall(pattern, s)
-
+    
+    # Eşleşen alt dizeleri yazdır
     if substrings:
         for substring in substrings:
             print(substring)
-        else:
-            print(-1)
+    else:
+        print(-1)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     s = input()
     find_vowel_substrings(s)
