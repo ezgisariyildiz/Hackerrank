1857bde977bba7c164ee15c8530237fddfaa502a
Ezgi Çağla Sarıyıldız <e.sariyildiz2018@gtu.edu.tr>
2024-03-15 15:50:38 +0300

    15.03.2024

--- SQL/Basic Select/weather_observation_station_11.sql
+++ SQL/Basic Select/weather_observation_station_11.sql
@@ -5,7 +5,7 @@ WHERE (NOT(CITY  Like 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'O%' OR CITY LIKE 'I%'
       (NOT (CITY LIKE '%a' OR CITY  LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u'))
 ORDER BY CITY;
 
--- ########### VEYA İKİNCİ OLARAK AŞAĞIDAKİ KODU KULLANABİLİRİZ ###########
+-- ########### VEYA İKİNCİ OLARAK AŞAĞIDAKİ KODU KULLANABİLİRİZ ############
 
 
 SELECT DISTINCT CITY
