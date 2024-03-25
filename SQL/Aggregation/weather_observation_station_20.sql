39f32966fbb92cc2459fb432c0ecf069ae98c472
Ezgi Çağla Sarıyıldız <e.sariyildiz2018@gtu.edu.tr>
2024-03-25 16:48:06 +0300

    25.03.2024

---
+++ SQL/Aggregation/weather_observation_station_20.sql
@@ -0,0 +1,15 @@
+SELECT 
+    ROUND(
+        AVG(LAT_N), 
+        4
+    ) AS median
+FROM (
+    SELECT 
+        LAT_N, 
+        ROW_NUMBER() OVER (ORDER BY LAT_N) AS row_num, 
+        COUNT(*) OVER () AS total_rows
+    FROM 
+        STATION
+) AS sub
+WHERE 
+    row_num IN ((total_rows + 1) / 2, (total_rows + 2) / 2);
